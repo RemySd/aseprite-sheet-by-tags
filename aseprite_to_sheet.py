7d90cf22c15fe6e3f9c7abc8d349c9d6efeb27b1
Remy <remy.sainsard@gmail.com>
2022-12-01 15:46:59 +0100

    Exclusion system + default_path by argument command

--- aseprite_to_sheet.py
+++ aseprite_to_sheet.py
@@ -1,28 +1,34 @@
 import os
 import subprocess
+import sys
 
 # Macos default location
 binaryAseLocation="/Applications/Aseprite.app/Contents/MacOS/aseprite"
 
-# Avoir une notion d'exclusion
-
-# Avoir une notion de build custom
-
 # Default to fetch files
-defaultPath = './Assets'
+default_path = './Assets/'
+
+if len(sys.argv) >= 2:
+    default_path = sys.argv[1]
+
+# Exclusions files
+text_file = open("aseprite_to_exclude.txt", "r")
+lines = text_file.readlines()
+files_to_exlude = [default_path + item.replace('\n', '') for item in lines]
 
 # To create sprite sheet from tags we create temporaries ".aseprite" files by tags
-for root, directories, files in os.walk(defaultPath):
+for root, directories, files in os.walk(default_path):
     for file in files:
-        if '.aseprite' in file:
-            filePath = os.path.join(root, file)
+        filePath = os.path.join(root, file)
+        if '.aseprite' in filePath and filePath not in files_to_exlude:
+            print(filePath)
             subprocess.run(
                 [binaryAseLocation, "-b", filePath, "--save-as", filePath.replace(".aseprite", "_{tag}.tag.aseprite")],
                 capture_output=True
             )
 
 # Fetch all temporaries files and generate the sprite sheet
-for root, directories, files in os.walk(defaultPath):
+for root, directories, files in os.walk(default_path):
     for file in files:
         if '.tag.aseprite' in file:
             filePath = os.path.join(root, file)
